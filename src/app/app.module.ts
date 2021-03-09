863d57c7d69a8cd79bbde6dcc0346a32d7acc4df
Christophe <christophe.beaufils@outlook.com>
2021-03-10 00:37:04 +0100

    exercice3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { Exercice2Component } from "./exercice2/exercice2.component";
 import { Exercice1Component } from "./exercice1/exercice1.component";
+import { Exercice3Component } from './exercice3/exercice3.component';
 
 @NgModule({
-  declarations: [AppComponent, Exercice2Component, Exercice1Component],
+  declarations: [AppComponent, Exercice2Component, Exercice1Component, Exercice3Component],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
